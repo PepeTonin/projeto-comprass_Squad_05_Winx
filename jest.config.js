a03c23114175000c9669864fd18e71df9e2eff38
Jhimy Ferrari <souzaferrarik@gmail.com>
2023-10-17 15:51:02 -0300

    fix: test config

--- jest.config.js
+++ jest.config.js
@@ -4,7 +4,7 @@ module.exports = {
     
     "collectCoverage": true,
     "collectCoverageFrom": [
-      "**/*.{js,jsx}",
+      "**/*.{ts,tsx}",
       "!**/coverage/**",
       "!**/node_modules/**",
       "!**/babel.config.js",
