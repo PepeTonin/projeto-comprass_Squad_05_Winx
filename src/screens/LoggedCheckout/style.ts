5fb11c70df04ad3b6636a7a77263e8e444a0c931
gui18br <guilhermenovaislimaa@hotmail.com>
2023-10-17 16:31:26 -0300

    feat: add checkout-payment design.

--- src/screens/LoggedCheckout/style.ts
+++ src/screens/LoggedCheckout/style.ts
@@ -1,7 +1,60 @@
 import { StyleSheet } from "react-native";
+import { colors, fontFamilies, fontSizes } from "../../styles/globalStyles";
 
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
+    marginTop: 30,
+  },
+  backButton: {
+    top: 4,
+    left: 7,
+  },
+  header: {
+    alignSelf: "center",
+    flexDirection: "row",
+    bottom: 25,
+  },
+  textTitle: {
+    fontFamily: fontFamilies.extraBoldOpenSans,
+    fontSize: fontSizes.m,
+  },
+  textTitleButton: {
+    fontFamily: fontFamilies.boldOpenSans,
+    fontSize: fontSizes.s,
+    left: 16,
+    marginBottom: 15,
+  },
+  buttonBoxes: {
+    alignItems: "center",
+    alignContent: "center",
+  },
+  box: {
+    width: 343,
+    height: 108,
+    borderRadius: 8,
+
+    shadowColor: colors.black_800,
+    elevation: 0.5,
+  },
+  textButton: {
+    position: "absolute",
+    fontSize: 14,
+    marginVertical: 10,
+    top: 10,
+    right: 20,
+    color: colors.red_500,
+    fontFamily: fontFamilies.semiBoldOpenSans,
+  },
+  textMiddleButton: {
+    position: "absolute",
+    color: colors.gray_500,
+    top: 40,
+    left: 20,
+  },
+  image: {
+    width: 100,
+    height: 53,
+    marginHorizontal: 10,
   },
 });
